62ec1749ec2e26de0ffa68f87de3c8ffb8a40f2e
Leon Coto <leon.coto@rbs.com>
2016-09-02 10:55:13 +0100

    Test trivial cases

--- test/table_test.js
+++ test/table_test.js
@@ -1,23 +1,33 @@
-import Table from '../src'
+import React from 'react'
+import SimpleTable from '../src'
 import { strictEqual } from 'assert'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
+import { jsdom } from 'jsdom'
+import assert from 'assert'
+
+global.document = jsdom('')
+global.window = document.defaultView
 
 describe('react-simple-table', () => {
 
-  it('should have something', () => {
-    strictEqual(Table, 'Table!', 'Not the same :(')
+  it('should throw if missing required properties', () => {
+    assert.throws(() => { mount(<SimpleTable />) }, Error, '`columns` is required')
   })
+
+  it('should render a trivial table with empty columns', () => {
+    const wrapper = mount(<SimpleTable columns={[]}/>)
+        , expected = mount(trivialTable())
+
+    assert.equal(wrapper.html(), expected.html(), 'Rendering ')
+  })
+
 })
 
-
-// the trivial table
-// rows = undefined
-// columns = [{}]
-// <table data-reactroot="">
-//   <thead>
-//     <tr>
-//       <th></th>
-//     </tr>
-//   </thead>
-//   <tbody></tbody>
-// </table>
+function trivialTable() {
+  return <table>
+    <thead>
+      <tr></tr>
+    </thead>
+    <tbody></tbody>
+  </table>
+}
