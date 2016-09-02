133df452876f705ad65e7e75b43ae1ae697fc9d0
Leon Coto <leon.coto@rbs.com>
2016-09-02 11:48:07 +0100

    Test component properties

--- test/table_test.js
+++ test/table_test.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import SimpleTable from '../src'
-import { strictEqual } from 'assert'
-import { shallow, mount } from 'enzyme'
+import { deepEqual, equal, throws } from 'assert'
+import { mount } from 'enzyme'
 import { jsdom } from 'jsdom'
-import assert from 'assert'
 
 global.document = jsdom('')
 global.window = document.defaultView
@@ -11,14 +10,25 @@ global.window = document.defaultView
 describe('react-simple-table', () => {
 
   it('should throw if missing required properties', () => {
-    assert.throws(() => { mount(<SimpleTable />) }, Error, '`columns` is required')
+    throws(() => { mount(<SimpleTable />) }, Error, '`columns` is required')
   })
 
   it('should render a trivial table with empty columns', () => {
     const wrapper = mount(<SimpleTable columns={[]}/>)
         , expected = mount(trivialTable())
+    equal(wrapper.html(), expected.html(), 'Rendering ')
+  })
 
-    assert.equal(wrapper.html(), expected.html(), 'Rendering ')
+  it('should set all known properties', () => {
+    const wrapper = mount(
+      <SimpleTable columns={[]} rows={[]} selected={[]} onSelect={() => {}} />
+    )
+    const { columns, rows, selected, onSelect } = wrapper.props()
+
+    deepEqual(columns, [], '`columns` should be an empty array')
+    deepEqual(rows, [], '`rows` should be an empty array')
+    deepEqual(selected, [], '`selected` should be an empty array')
+    equal(typeof onSelect, 'function', '`onSelect` should be `typeof function`')
   })
 
 })
